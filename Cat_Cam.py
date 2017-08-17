da653a9fe2f4242da450126c3bbdda8ee415531e
cosmic.engineer <cosmic.engineer@gmail.com>
2017-08-17 16:30:58 +0100

    restructured code to allow for threading of multiple cameras and enabling of camera servo. Will have to allow for port selection later

--- Cat_Cam.py
+++ Cat_Cam.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3
+
+import _thread
 import re, time, cv2, serial
 
 
@@ -21,7 +24,7 @@ class ServoController:
     def servo_read_position(self):
         self.ser.write(b'r')
         pos = str(self.ser.readline())
-        # First read is always empty, sometimes the first severl,
+        # First read is always empty, sometimes the first seversl,
         # so recusively call until numbers come back from arduino
         while pos == "b''":
             pos = self.servo_read_position()
@@ -43,14 +46,14 @@ Currently written to sweep the camera, wait and detect motion, only continue swe
 '''
 
 
-class SerialServo:
+class CameraController:
 
-    def camera_frame_grab(self, frameName, capture):
-        ret, frame = capture.read()
-        cv2.imshow(frameName, frame)
+    def camera_frame_grab(self):
+        ret, frame = self.cap.read()
+        cv2.imshow(self.frameName, frame)
         cv2.waitKey(1)
 
-    def camera_rotate(self, start, stop, capture, step_delay=0.2):
+    def camera_rotate(self, start, stop, step_delay=0.2):
         # If the stop angle bigger than start, increment, else decrement
         # If the stop angle is smaller, it cannot be negative, if it's bigger,
         # then it can't be bigger than 180
@@ -64,59 +67,63 @@ class SerialServo:
             direction = -1
 
         for pos in range(start, stop, direction):
-            self.camera_frame_grab('frame', capture)
-            print("af pos: " + str(pos))
-            sc.servo_set_position(pos)
+            self.camera_frame_grab()
+            self.sc.servo_set_position(pos)
             time.sleep(step_delay)
 
     def camera_sweep(self):
         self.camera_rotate(45, 180)
         self.camera_rotate(180, 45)
 
-    def camera_left(self, degrees, capture):
-        pos = sc.servo_read_position()
+    def camera_left(self, degrees):
+        pos = self.sc.servo_read_position()
         if pos <= 180:
             self.camera_rotate(pos, pos + degrees)
 
-    def camera_right(self, degrees, capture):
-        pos = sc.servo_read_position()
+    def camera_right(self, degrees):
+        pos = self.sc.servo_read_position()
         if pos > 0:
-            self.camera_rotate(pos, pos - degrees, capture)
+            self.camera_rotate(pos, pos - degrees)
 
     def detectMotion(self, duration):
         start_time = time.time()
         while (True):
             end_time = time.time()
+            # Wait detecting motion for duration, stop detecting once time has passed and no motion detected
             if(end_time - start_time) > duration and count == 1:
                 print("Finished recording " + str(duration))
                 break
-            # Capture frame-by-frame
+            # Capture frame-by-frame apply the background removal mask.
             ret, frame = self.cap.read()
             fgmask = self.fgbg.apply(frame)
 
+            # Generate and prepare the threshold image, and find contours with it.
             ret, thresh = cv2.threshold(fgmask, 127, 255, 0)
             thresh = cv2.dilate(thresh, None, iterations=2)
             im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
+            # Count contours with large areas, use them for movement simple detection
             count = 1
-            # loop over the contours
             for c in contours:
                 if cv2.contourArea(c) < 500:
                     continue
                 count += count
-                # (x, y, w, h) = cv2.boundingRect(c)
-                # cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            cv2.imshow('frame', frame)
+                # optionally draw bounding boxes around the detected contours
+                (x, y, w, h) = cv2.boundingRect(c)
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            cv2.imshow(self.frameName, frame)
             k = cv2.waitKey(60) & 0xff
             if k == 27:
                 break
 
-    def __init__(self):
-
-        self.cap = cv2.VideoCapture(1)
+    def __init__(self, camera, servo=0):
+        if servo:
+            self.sc = ServoController()
+        self.frameName = "Camera " + str(camera)
+        self.cap = cv2.VideoCapture(camera)
         self.fgbg = cv2.createBackgroundSubtractorMOG2()
         ret, frame = self.cap.read()
-        cv2.imshow('frame', frame)
+        cv2.imshow(self.frameName, frame)
 
     def __enter__(self):
         return self
@@ -126,35 +133,52 @@ class SerialServo:
         self.cap.release()
         cv2.destroyAllWindows()
 
+
+def dynamic_camera(cam):
+    with CameraController(cam, 1) as cc:
+        while(True):
+            cc.camera_right(25)
+            cc.detectMotion(20)
+            cc.camera_right(25)
+            cc.detectMotion(20)
+            cc.camera_right(25)
+            cc.detectMotion(20)
+            cc.camera_right(25)
+            cc.detectMotion(20)
+            cc.camera_right(25)
+            cc.detectMotion(20)
+            cc.camera_right(25)
+            cc.detectMotion(20)
+            cc.camera_left(25)
+            cc.detectMotion(20)
+            cc.camera_left(25)
+            cc.detectMotion(20)
+            cc.camera_left(25)
+            cc.detectMotion(20)
+            cc.camera_left(25)
+            cc.detectMotion(20)
+            cc.camera_left(25)
+            cc.detectMotion(20)
+            cc.camera_left(25)
+            cc.detectMotion(20)
+            cc.camera_left(25)
+            cc.detectMotion(20)
+            cc.camera_left(25)
+            cc.detectMotion(20)
+
+def static_camera(cam):
+    print("Static Cam")
+    with CameraController(cam) as cc:
+        while (True):
+            cc.detectMotion(20)
+
+
 if __name__ == "__main__":
-    with ServoController() as sc:
-        with SerialServo() as ss:
-            while(True):
-                ss.camera_right(25, ss.cap)
-                ss.detectMotion(20)
-                ss.camera_right(25, ss.cap)
-                ss.detectMotion(20)
-                ss.camera_right(25, ss.cap)
-                ss.detectMotion(20)
-                ss.camera_right(25, ss.cap)
-                ss.detectMotion(20)
-                ss.camera_right(25, ss.cap)
-                ss.detectMotion(20)
-                ss.camera_right(25, ss.cap)
-                ss.detectMotion(20)
-                ss.camera_left(25, ss.cap)
-                ss.detectMotion(20)
-                ss.camera_left(25, ss.cap)
-                ss.detectMotion(20)
-                ss.camera_left(25, ss.cap)
-                ss.detectMotion(20)
-                ss.camera_left(25, ss.cap)
-                ss.detectMotion(20)
-                ss.camera_left(25, ss.cap)
-                ss.detectMotion(20)
-                ss.camera_left(25, ss.cap)
-                ss.detectMotion(20)
-                ss.camera_left(25, ss.cap)
-                ss.detectMotion(20)
-                ss.camera_left(25, ss.cap)
-                ss.detectMotion(20)
+    try:
+        _thread.start_new_thread(dynamic_camera, (1, ))
+        _thread.start_new_thread(static_camera, (0, ))
+    except:
+        print("Error: unable to start thread")
+
+    while 1:
+        pass
